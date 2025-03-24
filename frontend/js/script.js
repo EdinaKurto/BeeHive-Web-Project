30ca7d463eeeba0c3cfc30549e1a228444d6266f
Edina Kurto <122160209+EdinaKurto@users.noreply.github.com>
2025-03-24 07:13:44 +0100

    initial

--- frontend/js/script.js
+++
@@ -1,8 +0,0 @@
-var app = $.spapp({
-    defaultView: "#dashboard",
-    templateDir: "../views/"
-
-});
-
-
-app.run();
--- frontend/utils/utils.js
+++
@@ -1,9 +0,0 @@
-const Utils = {
-    init_spapp: function () {
-        var app = $.spapp({
-            defaultView: "#dashboard",
-            templateDir: "frontend/views/"
-        });
-        app.run();
-    }
-}
